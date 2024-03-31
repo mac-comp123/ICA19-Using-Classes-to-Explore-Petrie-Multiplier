d9bbbc8b0bc55d95d6c1a0507d134f46dcbd6a7b
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-03-31 08:11:25 -0500

    Refactor

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ICA19: Using Classes to Explore Petrie Multipler
-See the instructions of the activity [here](https://docs.google.com/document/d/15fsa5LnZrcKhLk1dYOS6qFGdMph5i0EtHb2WjzO-JyE/preview).
+# ICA19: Using Classes to Explore Petrie Multiplier
+See the instructions of the activity [here](https://docs.google.com/document/d/11LtlZ96nu9cZKUS2238BwCsNRxmnJMdRuSk8XTr9MO8/preview).
---
+++ petrie_simulation.py
@@ -0,0 +1,117 @@
+"""
+Contains a simulation of the Petrie Multiplier that is based on classes.
+"""
+
+import random
+import math
+
+
+class Employee:
+    """
+    For this simulation, we only focus on the gender of an employee, and on
+    whether this employee is likely to make negative statements
+    towards the other gender.
+    """
+
+    def __init__(self, gender: str, will_comment):
+        """
+        Takes in the employee's gender and whether they comment, and it
+        saves those values to instance variables. It also initializes the
+        variable that holds the comments received by this employee to zero.
+        """
+        pass
+
+    def __str__(self):
+        """
+        Produces a printable string format for this employee.
+        """
+        return (self.gender.rjust(5)
+                + ": "
+                + str(self.comments_received)
+                + " sexist comments received")
+
+
+def print_employee_list(lst):
+    """
+    Given a list of employees, this method will print the details of each employee
+    by using the print() method
+    """
+    pass
+
+
+def create_employees(total_num):
+    """
+    Takes in the number of employees to make, builds and returns a list that contains
+    that many employees. It ensures that ~80% are men and the rest women.
+    """
+    pass
+
+
+def create_commenters(lst):
+    """
+    Given a list of employees, make 20% of each gender be sexist employees. This
+    method should not return anything.
+    """
+    pass
+
+
+def generate_comments(lst):
+    """
+    Given a list of employees, have each sexist employee give one sexist comment to
+    another employee of the opposite gender, chosen randomly. This method should
+    not return anything
+    """
+    pass
+
+
+def average_comments(lst):
+    """
+    Returns a tuple that represents the average amount of comments received for men and women
+    respectively. Return statement will be in the form (<avg_for_men>, <avg_for_women>)
+    """
+    pass
+
+
+def main():
+    """
+    Print out information about the average comments
+    received by men and women after a simulation has been run
+    """
+    num_employees_to_generate = 100
+    num_comment_rounds = 50
+
+    employee_list = create_employees(num_employees_to_generate)
+    create_commenters(employee_list)
+    for rounds in range(num_comment_rounds):
+        generate_comments(employee_list)
+
+    (men_avg, women_avg) = average_comments(employee_list)
+    print("  Men received on average ",   men_avg, "sexist comments")
+    print("Women received on average ", women_avg, "sexist comments")
+
+
+if __name__ == "__main__":
+    "<----- Test code for print_employee_list ----->"
+    lst = [Employee('Man', True),
+           Employee('Man', False),
+           Employee('Woman', True),
+           Employee('Woman', False)]
+    print_employee_list(lst)
+
+    "<----- Test code for create_employees ----->"
+    employees = create_employees(10)
+    print_employee_list(employees)
+
+    "<----- Test code for create_commenters ----->"
+    create_commenters(employees)
+    print_employee_list(employees)
+
+    "<----- Test code for generate_comments ----->"
+    generate_comments(employees)
+    print_employee_list(employees)
+
+    "<----- Test code for average_comments ----->"
+    print(average_comments(employees))
+
+    "<----- Run the simulation ----->"
+    # main()  # <-- KEEP THIS
